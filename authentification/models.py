a6cd4f8ab627e53c0ede5cdb9fb673051b07cf22
nastya-nizharadze <naniti@bk.ru>
2018-03-30 00:20:42 +0300

    just had finished developing of models, url parsing and now working on views

---
+++ authentification/models.py
@@ -0,0 +1,26 @@
+from __future__ import unicode_literals
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(label='Username', max_length=40, required=True, min_length=3)
+    password = forms.CharField(label='Password', required=True, min_length=8)
+
+    def save(self):
+        user = authenticate(**self.cleaned_data)
+        return user
+
+
+class SignUpForm(forms.Form):
+    first_name = forms.CharField(label='First name', max_length=40, required=False)
+    last_name = forms.CharField(label='Last name', max_length=40, required=False)
+    username = forms.CharField(label='Username', max_length=40, required=True, min_length=3)
+    email = forms.EmailField(required=True)
+    password = forms.CharField(label='Password', widget=forms.PasswordInput(), required=True, min_length=6)
+
+    def save(self):
+        user = User.objects.create_user(**self.cleaned_data)
+        user.save()
+        return user
---
+++ database/models.py
@@ -0,0 +1,45 @@
+from __future__ import unicode_literals
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+
+
+class Person (User):
+    is_banned = models.BooleanField(default=False)
+    popularity = models.PositiveSmallIntegerField(default=0)
+
+    def __str__(self):
+        return self.username
+
+
+class CategoryChallenge (models.Model):
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.name
+
+
+class PlotChallenge(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=500)
+    challenge_creator = models.ForeignKey(Person)
+
+    def __str__(self):
+        return self.name
+
+
+class Challenge(models.Model):
+    name = models.CharField(max_length=200, default='challenge_name')
+    category_challenge = models.ManyToManyField(CategoryChallenge, 'challenge_category')
+    pub_date = models.DateTimeField('date published', default=timezone.now)
+    edit_date = models.DateTimeField('date edited', default=timezone.now)
+    description = models.CharField(max_length=500)
+    popularity = models.PositiveSmallIntegerField(default=0)
+    cost = models.PositiveSmallIntegerField(default=100)
+    plot_challenge = models.ManyToManyField(PlotChallenge, 'plot_challenge')
+    challenge_creator = models.ForeignKey(Person)
+    difficulty = models.PositiveSmallIntegerField(default=100)
+
+    def __str__(self):
+        return self.name
