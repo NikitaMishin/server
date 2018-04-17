a77f4c7e1b963a2aa3744d99e0b83c809a19d9dd
nastya-nizharadze <naniti@bk.ru>
2018-04-17 21:31:14 +0300

    switch to mysql

--- server/settings.py
+++ server/settings.py
@@ -86,11 +86,14 @@ ASGI_APPLICATION = 'server.routing.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'dbs',
+        'USER': 'root',
+        'PASSWORD': 'fhvfutljy7',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
